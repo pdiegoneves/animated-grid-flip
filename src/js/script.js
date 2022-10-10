ef54b21764db1bf7b63d99bed04f76735e866bf7
Diego Neves <pdcassiano@gmail.com>
2022-10-09 22:51:19 -0300

    tentando transferir dados para JSON, dificuldade com array no json

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 const cards = document.querySelectorAll('.card')
+const dados = getData()
 
 cards.forEach((card) => {
   card.addEventListener('click', () => {
@@ -11,4 +12,27 @@ function minimizeAll() {
   cards.forEach((card) => {
     card.classList.remove('active')
   })
+}
+
+function render(card) {
+  return `
+  <article class="card">
+        <h2 class="title">${card.title}</h2>
+        <img
+          src="${card.image}"
+          alt=""
+        />
+        <p class="description">
+          ${card.description}
+        </p>
+      </article>`
+}
+
+async function getData() {
+  const config = {
+    headers: { Accept: 'application/json' },
+  }
+  const res = await fetch('../src/data/dados.json', config)
+  const data = await res.json()
+  return data
 }
